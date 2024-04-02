86daf1d4197957e6d37eab392c9e42c773b5cb0b
robson_leal <robson_leal@sicredi.com.br>
2024-04-02 20:25:00 -0300

    Commit inicial

---
+++ src/main/java/com/robsonleal/autobrilho/AutobrilhoApplication.java
@@ -0,0 +1,13 @@
+package com.robsonleal.autobrilho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutobrilhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutobrilhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/robsonleal/autobrilho/AutobrilhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robsonleal.autobrilho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutobrilhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
